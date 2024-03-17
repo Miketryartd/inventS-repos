93086154f8f4f28b5ac66985b4f55caa84803ac2
Miketryartd <142714918+Miketryartd@users.noreply.github.com>
2024-03-17 15:51:03 +0800

    home.js

---
+++ home.js
@@ -0,0 +1,9 @@
+function mobileC(){
+    const sideBar = document.getElementById('sidebar');
+
+    if (sideBar.style.display === 'block'){
+        sideBar.style.display = 'none';
+    } else {
+        sideBar.style.display = 'block';
+    }
+}
